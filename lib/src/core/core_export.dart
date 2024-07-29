5330e9dfce267c648527e8fe2ed70dabd6d7b3c3
samakunchan <badgehades@hotmail.com>
2024-07-30 00:18:20 +0200

    chore: gestion fichier

---
+++ lib/src/core/core_export.dart
@@ -0,0 +1,18 @@
+/// CORE EXPORT
+library;
+
+/// DTO
+/// AUTH TYPE
+export 'dtos/keycloak_auth_type_dto.dart';
+
+/// DTO
+/// CONFIGURATION
+export 'dtos/keycloak_conf_dto.dart';
+
+/// DTO
+/// TOKENS
+export 'dtos/keycloak_token_dto.dart';
+
+/// DTO
+/// REPOSITORIES
+export 'repositories/keycloak_repository.dart';
