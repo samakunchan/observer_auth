776d5433fa0aec1b35be25a125c026c611c6ba20
samakunchan <badgehades@hotmail.com>
2024-08-07 21:57:16 +0200

    feat(repository): user infos
    - Création de la requête http pour récupérer les infos utilisateurs.

--- lib/src/core/repositories/keycloak_repository.dart
+++ lib/src/core/repositories/keycloak_repository.dart
@@ -5,6 +5,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:observer_auth/library_observer_auth.dart';
+import 'package:observer_auth/src/core/dtos/user_infos_dto.dart';
 
 class KeycloakRepository {
   const KeycloakRepository({required this.keycloakConfDTO});
@@ -128,6 +129,44 @@ class KeycloakRepository {
         (json['scope'] as String).split(' '),
         <String, dynamic>{},
       );
+
+      return Right(result);
+    } on PlatformException catch (error) {
+      KeycloakAuthTypeDTO.isBusy = false;
+      return Left(error.toString());
+    } on HandshakeException catch (error) {
+      KeycloakAuthTypeDTO.isBusy = false;
+      return Left('On a une érreur de type Handshake : ${error.toString()}. Possibilité de résolution :'
+          ' Vous utilisez un localhost pour faire la requête et celui-ci est en https au lieu de http.');
+    } catch (error) {
+      KeycloakAuthTypeDTO.isBusy = false;
+      return Left('On a pas pu générer les tokens avec le code. Message : $error');
+    }
+  }
+
+  Future<Either<String, UserInfosDTO>> getUserInfos({
+    required KeycloakAuthTypeDTO keycloakAuthTypeDTO,
+    required KeycloakConfDTO keycloakConfDTO,
+    required bool isDevMode,
+  }) async {
+    KeycloakAuthTypeDTO.isBusy = true;
+    final Uri uri = !isDevMode
+        ? Uri.parse('${keycloakConfDTO.issuer}/authentication/user-infos')
+        : Uri.parse('http://localhost:3005/authentication/user-infos');
+
+    try {
+      /// On envoie les infos vers l'API
+      final http.Response httpResponse = await http.get(
+        uri,
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+          'Access-Control-Allow-Origin': '*',
+        },
+      );
+
+      /// On récupère la réponse de l'API
+      final Map<String, dynamic> json = jsonDecode(httpResponse.body) as Map<String, dynamic>;
+      final UserInfosDTO result = UserInfosDTO.fromJson(json);
 
       return Right(result);
     } on PlatformException catch (error) {
