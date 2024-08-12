11837781b1b3570f63646c921abb33b6a28dbfd5
samakunchan <badgehades@hotmail.com>
2024-08-12 03:30:04 +0200

    fix: class useless

--- lib/src/core/app.dart
+++
@@ -1,7 +0,0 @@
-import 'package:observer_auth/library_observer_auth.dart';
-
-class Auth {
-  static final KeycloakAuthTypeDTO keycloakAuthTypeDTO = KeycloakAuthTypeDTO.empty;
-  static final KeycloakConfDTO keycloakConfDTO = KeycloakConfDTO.empty;
-  static final KeycloakTokenDTO keycloakTokenDTO = KeycloakTokenDTO.empty;
-}
